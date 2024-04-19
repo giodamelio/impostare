ea94f945125bcfc574a64d7cad97f7f3e8f6ca3b
Giovanni d'Amelio <gio@damelio.net>
2024-04-19 16:10:20 -0700

    New config format

--- src/config.rs
+++ src/config.rs
@@ -1,25 +1,68 @@
-use std::collections::HashMap;
+use serde::{Deserialize, Deserializer};
 
-use serde::Deserialize;
-
-#[derive(Debug, Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct Config {
-    pub databases: Vec<Database>,
-    pub users: Vec<User>,
+    databases: Vec<Database>,
+    users: Vec<User>,
+    database_permissions: Vec<DatabasePermission>,
+    schema_permissions: Vec<SchemaPermission>,
+    table_permissions: Vec<TablePermission>,
 }
 
-#[derive(Debug, Deserialize)]
-pub struct Database {
-    pub name: String,
-    #[serde(default)]
-    pub extensions: Vec<String>,
+#[derive(Deserialize, Debug)]
+struct Database {
+    name: String,
+    extensions: Vec<String>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct User {
-    pub name: String,
-    pub password_file: String,
-    pub permissions: Option<HashMap<String, Permissions>>,
+    name: String,
+    password_file: String,
 }
 
-pub type Permissions = Vec<String>;
+#[derive(Deserialize, Debug)]
+struct DatabasePermission {
+    role: String,
+    permissions: Vec<String>,
+    databases: Vec<String>,
+    make_default: bool,
+}
+
+#[derive(Deserialize, Debug)]
+struct SchemaPermission {
+    role: String,
+    permissions: Vec<String>,
+    schemas: Vec<String>,
+    make_default: bool,
+}
+
+#[derive(Deserialize, Debug)]
+struct TablePermission {
+    role: String,
+    permissions: Vec<String>,
+    tables: Tables,
+    make_default: bool,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+enum Tables {
+    #[serde(deserialize_with = "all")]
+    All,
+    List(Vec<String>),
+}
+
+// Deserialize just the ALL half of the enum
+// See: https://github.com/serde-rs/serde/issues/1158)
+fn all<'de, D>(deserializer: D) -> Result<(), D::Error>
+where
+    D: Deserializer<'de>,
+{
+    #[derive(Deserialize)]
+    enum Helper {
+        #[serde(rename = "ALL")]
+        Variant,
+    }
+    Helper::deserialize(deserializer).map(|_| ())
+}
