5fa27fb07e3e7251b1973207fb842e03b088294c
Giovanni d'Amelio <gio@damelio.net>
2024-04-19 08:59:32 -0700

    Setup devenv for Rust

---
+++ devenv.nix
@@ -0,0 +1,14 @@
+_: {
+  languages.nix.enable = true;
+  languages.rust.enable = true;
+
+  pre-commit.hooks.cargo-check.enable = true;
+  pre-commit.hooks.clippy.enable = true;
+  pre-commit.hooks.rustfmt.enable = true;
+
+  difftastic.enable = true;
+
+  enterTest = ''
+    cargo test
+  '';
+}
