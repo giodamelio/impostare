501e89114488791a894ed79c21174b14815f7532
Giovanni d'Amelio <gio@damelio.net>
2024-04-22 15:31:26 -0700

    Add a simple flake that builds cli

--- flake.nix
+++ flake.nix
@@ -1,79 +1,21 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    systems.url = "github:nix-systems/default";
-    devenv.url = "github:cachix/devenv";
-    devenv.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "impostare";
+          version = "0.1.0";
 
-  outputs = {
-    self,
-    nixpkgs,
-    devenv,
-    systems,
-    ...
-  } @ inputs: let
-    forEachSystem = nixpkgs.lib.genAttrs (import systems);
-  in {
-    packages = forEachSystem (system: {
-      devenv-up = self.devShells.${system}.default.config.procfileScript;
-    });
-
-    devShells =
-      forEachSystem
-      (system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit (pkgs) lib;
-      in {
-        default = devenv.lib.mkShell {
-          inherit inputs pkgs;
-          modules = [
-            {
-              languages.nix.enable = true;
-              languages.rust.enable = true;
-
-              pre-commit.hooks = {
-                # Rust hooks
-                cargo-check.enable = true;
-                clippy.enable = true;
-                rustfmt.enable = true;
-
-                # Nix hooks
-                alejandra.enable = true;
-                deadnix.enable = true;
-                statix.enable = true;
-                nil.enable = true;
-              };
-
-              difftastic.enable = true;
-
-              enterTest = ''
-                cargo test
-              '';
-
-              services.postgres = {
-                enable = true;
-                package = pkgs.postgresql_15;
-                extensions = extensions: [
-                  extensions.timescaledb
-                ];
-                initialScript = ''
-                  CREATE USER postgres SUPERUSER;
-                '';
-                settings.shared_preload_libraries = "timescaledb";
-              };
-
-              packages =
-                lib.optionals
-                pkgs.stdenv.isDarwin
-                (with pkgs.darwin.apple_sdk; [
-                  frameworks.CoreFoundation
-                  frameworks.Security
-                  frameworks.SystemConfiguration
-                ]);
-            }
-          ];
+          src = ./.;
+          cargoHash = "sha256-+GCZM0YoRM+0XqlX9cZwY1LjWTBWD2zW4AQgst86jck=";
         };
-      });
-  };
+      };
+      flake = {};
+    };
 }
