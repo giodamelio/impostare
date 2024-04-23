d7661af62c8efbdd52583eb66d668d54f9ac716f
Giovanni d'Amelio <gio@damelio.net>
2024-04-22 18:28:00 -0700

    First real NixOS VM test of impostare

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       imports = [];
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
-        packages.default = pkgs.rustPlatform.buildRustPackage {
+        packages.impostare = pkgs.rustPlatform.buildRustPackage {
           pname = "impostare";
           version = "0.1.0";
 
@@ -20,11 +20,51 @@
             name = "hello-world";
 
             nodes.machine = { config, pkgs, ... }: {
+              services.postgresql = {
+                enable = true;
+              };
+
+              environment.etc.connection-details.text = ''
+                host=/run/postgresql user=postgres
+              '';
+
+              environment.etc."database-settings.toml".text = ''
+                [[databases]]
+                name = "db1"
+
+                [[databases]]
+                name = "db2"
+
+                [[users]]
+                name = "root"
+              '';
+
+              systemd.services.impostare = {
+                enable = true;
+                wantedBy = ["default.target"];
+                requires = ["postgresql.service"];
+                after = ["postgresql.service"];
+                serviceConfig = {
+                  Type = "oneshot";
+                  User = "postgres";
+                };
+                script = ''
+                  ${self'.packages.impostare}/bin/impostare \
+                    /etc/connection-details \
+                    /etc/database-settings.toml
+                '';
+              };
+
               system.stateVersion = "23.11";
             };
 
             testScript = ''
               machine.wait_for_unit("default.target")
+              machine.wait_for_open_port(5432)
+
+              # Make sure the databases exist
+              machine.succeed("psql -lqt | grep db1")
+              machine.succeed("psql -lqt | grep db2")
             '';
           };
         };
